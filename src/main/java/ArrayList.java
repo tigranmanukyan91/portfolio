008b01900766d53a8ffc1e4d8c23568fec9d1db5
Tigran <tmanukian91@gmail.com>
2023-11-03 17:11:11 +0400

    Array List and LinkedList

---
+++ src/main/java/ArrayList.java
@@ -0,0 +1,10 @@
+public class ArrayList {
+    public static void main(String[] args) {
+
+        java.util.ArrayList<Integer> x = new java.util.ArrayList<>();
+        for (int i = 0; i < 10 ; i++) {
+            x.add(i);
+        }
+
+    }
+}
---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,20 @@
+
+public class LinkedList {
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+
+        System.out.println(linkedList);
+        System.out.println(linkedList.get(0));
+
+        linkedList.remove(0);
+        System.out.println(linkedList);
+
+    }
+
+
+}
